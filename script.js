6cbb4464d10e024ec5804e5cc0359640efde1b59
Marco Lopes <79868586+MarcoLopes389@users.noreply.github.com>
2022-01-26 18:55:53 -0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const root = document.getElementById('root')
-const button = document.getElementById('button')
-const text = document.createElement('p')
-const textNode = document.createTextNode('Teste')
-text.appendChild(textNode)
-
-button.addEventListener('click', () => {
-    root.appendChild(text)
-})
